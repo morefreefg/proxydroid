425ae55a6eaef93e77568c0803371c0c2aa0d636
bwelco <morefreefg@gmail.com>
2018-08-03 14:48:43 +0800

    add netty server demo

--- src/main/java/me/bwelco/Main.java
+++ src/main/java/me/bwelco/Main.java
@@ -1,8 +1,11 @@
 package me.bwelco;
 
+import me.bwelco.netty.NettyServer;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Server().start(1080);
+//        new Server().start(1080);
+        new NettyServer(1080).startServer();
     }
 }
