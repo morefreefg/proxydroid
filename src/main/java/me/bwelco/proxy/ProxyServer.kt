f32a99bd3f733a86a02a82e6b8a990ab0a303472
bwelco <morefreefg@gmail.com>
2018-08-07 23:03:37 +0800

    add logger

--- src/main/java/me/bwelco/proxy/ProxyServer.kt
+++ src/main/java/me/bwelco/proxy/ProxyServer.kt
@@ -6,13 +6,17 @@ import io.netty.channel.socket.nio.NioServerSocketChannel
 import io.netty.handler.logging.LogLevel
 import io.netty.handler.logging.LoggingHandler
 import me.bwelco.proxy.s5.SocksServerInitializer
+import org.apache.commons.logging.LogFactory
+import java.net.Proxy
 import java.net.Socket
 
 fun main(args: Array<String>) {
-
+    ProxyServer().start(1080)
 }
 
 class ProxyServer {
+
+    val logger = LogFactory.getLog(ProxyServer::class.java)
 
     fun start(port: Int, onConnectListener: (Socket) -> Unit = {}) {
         val bossGroup = NioEventLoopGroup(1)
@@ -23,6 +27,7 @@ class ProxyServer {
                     .channel(NioServerSocketChannel::class.java)
                     .handler(LoggingHandler(LogLevel.INFO))
                     .childHandler(SocksServerInitializer(onConnectListener))
+            logger.debug("start server at: $port")
             b.bind(port).sync().channel().closeFuture().sync()
         } finally {
             bossGroup.shutdownGracefully()
