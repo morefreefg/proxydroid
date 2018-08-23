2370ef22bf1eb80209c08658f1f68835dfd49b93
bwelco <morefreefg@gmail.com>
2018-08-23 13:22:27 +0800

    Remove: remove http/s interceptor

--- src/main/java/me/bwelco/proxy/http/HttpInterceptorHandler.kt
+++ src/main/java/me/bwelco/proxy/http/HttpInterceptorHandler.kt
@@ -3,17 +3,12 @@ package me.bwelco.proxy.http
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.handler.codec.http.FullHttpRequest
-import me.bwelco.proxy.upstream.HttpsUpstream
 
 class HttpInterceptorHandler(val httpInterceptor: HttpInterceptor) : ChannelInboundHandlerAdapter() {
     override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
         if (msg is FullHttpRequest) {
-                ctx.pipeline().addAfter(
-                        "HttpInterceptorHandler",
-                        "HttpInterceptorHandler",
-                        MitmHandler(httpInterceptor))
-
-                ctx.pipeline().addBefore("HttpInterceptorHandler", "CorrectCRLFHander", CorrectCRLFHander())
+            ctx.pipeline().addAfter("HttpInterceptorHandler", "MitmHandler", MitmHandler(httpInterceptor))
+            ctx.pipeline().addBefore("MitmHandler", "CorrectCRLFHander", CorrectCRLFHander())
         }
 
         ctx.fireChannelRead(msg)
